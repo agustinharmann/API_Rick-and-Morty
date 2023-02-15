9100d92320f3dd6a7ca98caa77ffca0ec0317e70
agustinharmann <agustinharmann@gmail.com>
2023-02-15 00:08:16 -0300

    add maquetation cards and finished consume first page

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './styles.css';
+
+const Card = ({ results }) => {
+  // console.log(results);
+  // console.log(results.results);
+  // const character = results.results;
+  // console.log(character);
+  console.log(results);
+  return (
+    <div>
+      {/* {
+        character.map(c =>
+          <div className='card' key={c.id}>
+            {c.name}
+          </div>
+        )
+      } */}
+      <div className='card'>
+        <div className="container_img--card">
+          <img className='img--card' src={results.image} alt="" />
+        </div>
+        <div className="container_basic-info--card">
+          <p>{results.name}</p>
+        </div>
+      </div>
+      {/* {
+        console.log(character[0])
+      } */}
+    </div>
+  );
+};
+
+export { Card };
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card } from '../Card';
 import './styles.css';
 
-const Home = ()=> {
-  return(
-    <div>
-      <h1>Soy un Home</h1>
+const Home = () => {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pagination, setPagination] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch('https://rickandmortyapi.com/api/character')
+      const data = await resp.json()
+      // console.log(data);
+      // console.log('hola desde el home');
+      setData(data);
+      setLoading(false);
+    }
+
+    fetchData()
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  const { info, results } = data;
+  
+  //MANDAR TODO COMO HELPER
+
+  // console.log(data);
+  // console.log(info);
+  // console.log(results);
+
+  // const {characters} = results;
+  // console.log(characters);
+
+  return (
+    <div className="home">
+      <div className='cards__home'>
+        {
+          results.map((results, index) =>
+            <div className='content_card--home' key={index}>
+              <Card results={results} />
+            </div>)
+        }
+      </div>
+      <div className="pagination__home">
+        soy una paginacion
+      </div>
     </div>
   );
 };
 
-export {Home};
+export { Home };
